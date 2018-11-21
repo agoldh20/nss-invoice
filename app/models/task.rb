86e9cd4a42111c5665092bb8067a40c5f55d5981
agoldh20 <a.goldh20@gmail.com>
2018-11-21 14:04:55 -0600

    fixed line totals displaying

--- app/models/task.rb
+++ app/models/task.rb
@@ -1,10 +1,3 @@
 class Task < ApplicationRecord
   belongs_to :invoice
-
-  def total_price
-    puts "=================================="
-    puts "ANSWER GOES HERE"
-    puts "=================================="
-    # invoices.all.pluck(:line_total).map { |total| total.to_i }.reduce(:+)
-  end
 end
