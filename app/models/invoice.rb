9976c32792fa69285059984286b6e33c57ea1650
Adam Goldwater <agoldwater@Adams-MacBook-Pro.local>
2018-11-26 19:13:34 -0600

    refactor pdf:

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -8,21 +8,21 @@ class Invoice < ApplicationRecord
 
   def build_invoice_pdf
     pdf = Prawn::Document.new
-    pdf.image open("./app/assets/images/pdf_header.png"),  :width => 550, :height => 100
+    pdf.image open("./app/assets/images/pdf_header.png"), width: 550, height: 100
     pdf.move_down 20
-    pdf.formatted_text [{:text =>"Customer: ", :styles => [:bold]}]
+    pdf.formatted_text [{text: "Customer: ", styles: [:bold]}]
     pdf.text "#{customer.full_name}"
     pdf.text "#{customer.street_number} #{customer.street_name} #{customer.unit}"
     pdf.text "#{customer.city}, #{customer.state} #{customer.zip}"
     pdf.text "#{customer.home_phone} #{customer.cell_phone}"
     pdf.text "#{customer.email}"
     pdf.move_down 20
-    pdf.formatted_text [{:text =>"Job: ", :styles => [:bold]}]
+    pdf.formatted_text [{text: "Job: ", styles: [:bold]}, {text: "1000#{id}"}]
     tasks.each do |task|
       pdf.text "#{task.room.upcase}: #{task.info}. $#{task.line_total}"
     end
     pdf.move_down 20
-    pdf.formatted_text [{:text =>"Total: ", :styles => [:bold]}, {:text => "$#{total_price}"}]
+    pdf.formatted_text [{text: "Total: ", styles: [:bold]}, {text: "$#{total_price}"}]
 
     pdf
   end
